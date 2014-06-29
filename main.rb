ab5e26b9123ab71146828f9bf4ff23620fc93325
sunilunka <sunil.unka@gmail.com>
2014-06-29 20:36:50 +1200

    Initial commit of Lighthouse Labs FizzBuzz program

---
+++ main.rb
@@ -0,0 +1,25 @@
+class FizzBuzz
+	def initialize(x,y)
+		@num_arr = [ ]
+		for i in x..y
+			@num_arr.push(i)
+		end
+	end
+
+	def pnt_num 
+		@num_arr.each do |i|
+			if i % 15 == 0
+				puts "#{i} FizzBuzz"
+			elsif i % 5 == 0
+				puts "#{i} Buzz"
+			elsif i % 3 == 0
+				puts "#{i} Fizz"
+			else
+				puts "#{i}"
+			end
+		end
+	end
+
+end
+
+FizzBuzz.new(1,100).pnt_num
